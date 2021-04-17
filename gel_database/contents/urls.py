5697d9db42b92ec577d4a73671de719907a9bd45
Debby-zhou <debby940406@gmail.com>
2021-04-17 17:34:32 +0800

    experiment/ tables layout update

--- gel_database/contents/urls.py
+++ gel_database/contents/urls.py
@@ -27,12 +27,7 @@ urlpatterns = [
     url('^template/$',views.showtemplate),
     url('^experiment/$',views.showexperiment, name="experiment"),
     url('^experiment/result/',views.show_experiment_result, name="result"),
-    #url('^experiment/score200317/', views.score200317, name="score200317"),
-    #url('^experiment/score201016/', views.score201016, name="score201016"),
-    #url('^experiment/ctvalues200317/', views.ctvalues200317, name="ctvalues200317"),
-    #url('^experiment/foldchanges200317/', views.foldchanges200317, name="foldchanges200317"),
-    #url('^experiment/parameter200317/', views.parameter200317, name="parameter200317"),
-    #url('^experiment/parameter201016/', views.parameter201016, name="parameter201016"),
+    
     
 ]
 
--- gel_database/gel_database/urls.py
+++ gel_database/gel_database/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path, include
 from contents import views
+from django.conf.urls import url
 admin.autodiscover()
 urlpatterns = [
     path('admin/', admin.site.urls),
